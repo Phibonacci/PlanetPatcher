75fc6d3eda9de39502d3eee8c28834bfff2b5210
Paul-Maxime <github@paulmaxime.fr>
2020-02-02 00:32:07 +0100

    Add gravity, remove buttons to the main menu

--- src/scenes/main-menu-scene.mjs
+++ src/scenes/main-menu-scene.mjs
@@ -10,9 +10,11 @@ export default class MainMenuScene extends Phaser.Scene {
 
 		this.cameras.main.setBackgroundColor(Phaser.Display.Color.GetColor(200, 0, 0))
 
-		this.load.image(`planet-core`, `assets/chunks/core.png`)
+		this.load.image('planet-core', 'assets/chunks/core.png')
+		this.load.json('core-hitbox', 'assets/chunks/core_hitbox.json')
 		for (let i = 1; i <= PLANET_CHUNK_COUNT; ++i) {
 			this.load.image(`planet-chunk${i}`, `assets/chunks/chunk${i}.png`)
+			this.load.json(`chunk${i}-hitbox`, `assets/chunks/chunk${i}_hitbox.json`)
 		}
 
 		this.load.audio('mecha-academy', 'assets/musics/mecha-academy.ogg')
@@ -23,27 +25,30 @@ export default class MainMenuScene extends Phaser.Scene {
 
 		const centerX = this.game.config.width / 2
 		const centerY = this.game.config.height / 2
-		this.planetCore = this.add.image(centerX, centerY, 'planet-core')
+		this.planetCore = this.matter.add.image(centerX, centerY, 'planet-core', null, {
+			shape: this.cache.json.get(`core-hitbox`).core,
+			ignorePointer: true,
+			isStatic: true,
+		})
 
 		this.chunks = []
 		for (let i = 1; i <= PLANET_CHUNK_COUNT; ++i) {
 			const x = centerX + (centerX / 2) * Math.cos(Math.PI * 2 * i / PLANET_CHUNK_COUNT)
 			const y = centerY + (centerY / 2) * Math.sin(Math.PI * 2 * i / PLANET_CHUNK_COUNT)
-			this.chunks.push(this.add.image(x, y, `planet-chunk${i}`))
+			const shape = this.cache.json.get(`chunk${i}-hitbox`)[`chunk${i}`]
+			this.chunks.push(this.matter.add.image(x, y, `planet-chunk${i}`, null, {
+				shape,
+			}))
 		}
-
-		this.add.text(this.game.config.width / 2, this.game.config.height / 4, 'Planet Patcher (TODO: logo)')
-		this.startButton = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'Play (TODO: triangle)')
-
-		this.loadingNextScene = false
-		this.startButton.setInteractive({ useHandCursor: true })
-		this.startButton.on('pointerdown', pointer => this.onStartButtonClick(pointer))
 
 		this.cameras.main.setBackgroundColor(0)
 		this.cameras.main.fadeIn(1000, 200, 0, 0)
 
 		this.music = this.sound.add('mecha-academy', { volume: 0.3 })
 		this.music.play({ loop: true, seek: 1.5 })
+
+		this.matter.add.mouseSpring()
+		this.matter.world.setBounds(0, 0, game.config.width, game.config.height)
 	}
 
 	onStartButtonClick(pointer) {
@@ -59,5 +64,9 @@ export default class MainMenuScene extends Phaser.Scene {
 	}
 
 	update(timestamp, elapsed) {
+		for (const chunk of this.chunks) {
+			chunk.setAngle(0)
+			chunk.setAngularVelocity(0)
+		}
 	}
 }
